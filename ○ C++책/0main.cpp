fe9cab802e00c9f3ba30caf15e81205fbea84138
KimJinHwan <wlsghks0616@naver.com>
2023-08-14 04:55:41 +0900

    8.14

--- ○ C++책/0main.cpp
+++ ○ C++책/0main.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-#include <map>
-#include <string>
 
 int main() {
-    std::map<std::string, int> dict;
+    int x = 5;
+    int y = 10;
 
-    dict["수학"] = 100;
-    dict["영어"] = 70;
-    dict["국어"] = 50;
+    // 외부 변수 x를 참조로 캡처하고 매개변수 b를 받아 덧셈 수행
+    auto add_and_print = [&x](int a, int b) -> int {
+        int result = a + b + x;
+        // return result;
+        std::cout << "Result: " << result << std::endl;
+    };
 
-    std::string subject_to_find = "영어";
-    auto range = dict.equal_range(subject_to_find);
-
-    std::cout << "Scores for the subject " << ":" << subject_to_find << std::endl;
-    for (auto it = range.first; it != range.second; ++it) {
-        std::cout << "Score: " << it->second << std::endl;
-    }
+    add_and_print(3, y);
 
     return 0;
 }
