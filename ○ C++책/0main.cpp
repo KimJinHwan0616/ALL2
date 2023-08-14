798be4c560eaa5a7c1d65a344320d35806d5a395
KimJinHwan <wlsghks0616@naver.com>
2023-08-15 03:59:41 +0900

    8.15

--- ○ C++책/0main.cpp
+++ ○ C++책/0main.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
+#include <memory>
 
-class Bread {
-private:
-    int quantity;
-
+class MyClass {
 public:
-    Bread(int quantity) {
-        this->quantity = quantity;
+    MyClass(int value) : data(value) {
+        std::cout << "MyClass Constructor with value: " << data << std::endl;
     }
 
-    int getQuantity() {
-        return this->quantity;
+    ~MyClass() {
+        std::cout << "MyClass Destructor for value: " << data << std::endl;
     }
+
+    void PrintValue() {
+        std::cout << "Value: " << data << std::endl;
+    }
+
+private:
+    int data;
 };
 
 int main() {
-    Bread bread1(5);  // Bread 객체 생성, 빵의 수량은 5개
-    Bread bread2(10); // Bread 객체 생성, 빵의 수량은 10개
+    std::shared_ptr<MyClass> ptr1 = std::make_shared<MyClass>(10);
 
-    std::cout << "Bread 1 quantity: " << bread1.getQuantity() << std::endl;
-    std::cout << "Bread 2 quantity: " << bread2.getQuantity() << std::endl;
+    // ptr1이 참조하는 객체의 PrintValue 호출
+    ptr1->PrintValue();
+
+    // ptr1.reset()을 호출하여 ptr1이 다른 객체나 nullptr을 가리키도록 함
+    ptr1.reset();
+
+    std::cout << "After ptr1.reset()" << std::endl;
+
+    // ptr1.reset()을 호출했으므로 ptr1은 nullptr을 가리킴
+    if (ptr1 == nullptr) {
+        std::cout << "ptr1 is nullptr" << std::endl;
+    }
+
+    std::shared_ptr<MyClass> ptr2 = std::make_shared<MyClass>(20);
+
+    // ptr2가 ptr1을 가리키도록 함
+    ptr1 = ptr2;
+
+    std::cout << "After assigning ptr2 to ptr1" << std::endl;
+
+    // ptr1이 ptr2가 가리키는 객체의 PrintValue 호출
+    ptr1->PrintValue();
 
     return 0;
 }
