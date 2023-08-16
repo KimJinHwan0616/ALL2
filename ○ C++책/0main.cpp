7c5a919a31ab77bfef9b6152502eeb86f231c484
jhkim8 <jhkim8@swm.ai>
2023-08-16 17:32:34 +0900

    8.16

--- ○ C++책/0main.cpp
+++ ○ C++책/0main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
 
 int main() {
-    std::string cppString = "Hello, World!";
-    const char* cString = cppString.c_str();
-
-    printf("%s\n", cString);
-
+    // double value = 3.14159265358979323846;
+    std::cout << std::setprecision(3) << 5.151515 << std::endl;
     return 0;
 }
