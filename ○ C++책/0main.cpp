a85bb66cf51146632a9d60e26880f5bb28038c60
KimJinHwan <wlsghks0616@naver.com>
2023-08-20 03:45:43 +0900

    8.20

--- ○ C++책/0main.cpp
+++ ○ C++책/0main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include <iomanip>
+#include <atomic>
+#include <thread>
 
 int main() {
-    // double value = 3.14159265358979323846;
-    std::cout << std::setprecision(3) << 5.151515 << std::endl;
+    std::atomic<int> value = 0;  // 정수형의 std::atomic 변수 생성 및 초기화
+
+    std::cout << "Initial value: " << value.load() << std::endl;
+
+    std::thread t1([&]() {
+        value.store(10, std::memory_order_relaxed);  // 값을 10으로 저장
+    });
+
+    std::thread t2([&]() {
+        value.store(20, std::memory_order_relaxed);  // 값을 20으로 저장
+    });
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Final value: " << value.load() << std::endl;
+
     return 0;
 }
