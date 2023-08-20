0f9381c48d17cf48be22fd185a39a4d45b56c257
KimJinHwan <wlsghks0616@naver.com>
2023-08-20 23:03:40 +0900

    8.20

--- ○ C++책/0main.cpp
+++ ○ C++책/0main.cpp
@@ -2,23 +2,27 @@
 #include <atomic>
 #include <thread>
 
+int r1 = 0, r2 = 0;
+std::atomic<int> x(0), y(0);
+
+void thread1() {
+    r1 = y.load(std::memory_order_relaxed); // A
+    x.store(r1, std::memory_order_relaxed); // B
+}
+
+void thread2() {
+    r2 = x.load(std::memory_order_relaxed); // C
+    y.store(42, std::memory_order_relaxed); // D
+}
 int main() {
-    std::atomic<int> value = 0;  // 정수형의 std::atomic 변수 생성 및 초기화
-
-    std::cout << "Initial value: " << value.load() << std::endl;
-
-    std::thread t1([&]() {
-        value.store(10, std::memory_order_relaxed);  // 값을 10으로 저장
-    });
-
-    std::thread t2([&]() {
-        value.store(20, std::memory_order_relaxed);  // 값을 20으로 저장
-    });
+    std::thread t1(thread1);
+    std::thread t2(thread2);
 
     t1.join();
     t2.join();
 
-    std::cout << "Final value: " << value.load() << std::endl;
+    std::cout << "r1: " << r1 << ", r2: " << r2 << std::endl;
+    std::cout << "Final x: " << x.load() << ", Final y: " << y.load() << std::endl;
 
     return 0;
 }
