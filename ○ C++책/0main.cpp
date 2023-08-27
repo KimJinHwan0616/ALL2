5d7b1f7ff7c06b28ae4c4ea46a4c8f96d272e237
KimJinHwan <wlsghks0616@naver.com>
2023-08-28 03:43:44 +0900

    8.28

--- ○ C++책/0main.cpp
+++ ○ C++책/0main.cpp
@@ -1,37 +1,60 @@
 #include <iostream>
-using namespace std;
 
-// explicit를 사용하지 않은 경우
-class ImplicitClass {
+class OrangeBox {
 public:
-    int value;
+    OrangeBox();    // 생성자 선언
+    ~OrangeBox();   // 소멸자 선언
 
-    ImplicitClass(int v) : value(v) {}
+    void Add(int addorangeBox);     // 오렌지를 넣는다.
+    void Del(int delorangeBox);     // 오렌지를 꺼낸다.
+    void Empty();       // 상자를 비운다.
+    int GetTotal() {    // 오렌지의 총 개수를 반환한다.
+        return total;
+    }
+
+private:
+    int total;
 };
 
-void printImplicit(ImplicitClass obj) {
-    cout << obj.value << endl;
+// 생성자 정의
+OrangeBox::OrangeBox()    
+{
+    total = 0;      // 멤버변수 초기화
 }
 
-// explicit를 사용한 경우
-class ExplicitClass {
-public:
-    int value;
-
-    explicit ExplicitClass(int v) : value(v) {}
-};
-
-void printExplicit(ExplicitClass obj) {
-    cout << obj.value << endl;
+// 소멸자 정의
+OrangeBox::~OrangeBox() 
+{
+    std::cout << "오렌지 상자 오브젝트 임무 완료";
 }
 
-int main() {
-    ImplicitClass implicitObj = 42;  // 암시적 형변환 발생
-    // ExplicitClass explicitObj = 42;  // 명시적 형변환 필요
 
-    printImplicit(implicitObj);  // 암시적 형변환
-    // 아래 줄은 컴파일 에러를 발생시킵니다.
-    //printExplicit(42);  // 명시적 형변환 필요
+// 멤버 함수
 
-    return 0;
+void OrangeBox::Add(int addorange) {
+    total += addorange;
+    if (total > 100) total = 100;
 }
+
+void OrangeBox::Del(int delorange) {
+    total -= delorange;
+    if (total < 0) Empty();
+}
+
+void OrangeBox::Empty() {
+    total = 0;
+}
+
+int main()
+{
+    OrangeBox myOrangeBox;      // 인스턴스 생성 OR 생성자 호출
+    myOrangeBox.Empty();     // 제일 먼저 상자를 비운다.
+
+    myOrangeBox.Add(5);
+    myOrangeBox.Del(2);
+
+    // myOrangeBox.total = 10;
+
+    std::cout << "상자 속의 오렌지 : " << myOrangeBox.GetTotal() << std::endl;
+}
+
